193a25df5a158cadff4236a143db2b2ba73bc5ac
Juan Pablo Mora <juampa@MacBook-Pro-de-Juan.local>
2015-04-10 00:08:37 +0200

    Plot1 finish

--- plot1.R
+++ plot1.R
@@ -2,7 +2,13 @@
 data = read.csv("household_power_consumption.txt", header = TRUE, sep = ";"  , na.strings=c("?"))
 
 # Filter between two dates.
-data <- subset(data, as.Date(data$Date, "%d/%m/%Y") >= '2007-02-01' & as.Date(data$Date, "%d/%m/%Y") <= '2007-02-02')
+data <- subset(data, as.Date(data$Date, "%d/%m/%Y") >= as.Date('2007-02-01', "%Y-%m-%d") & as.Date(data$Date, "%d/%m/%Y") <= as.Date('2007-02-02', "%Y-%m-%d"))
 
-# First try
-hist(data$Global_active_power)
+#Prepare a png file
+png("plot1.png",width=480,height=480,units="px")
+
+# Plot the hist
+hist(data$Global_active_power, col="red", main="Global Active Power", xlab = "Global Active Power (kilowatts)", xaxp  = c(0, 6, 3), xlim = c(0,8), yaxp = c(0,1200, 6))
+
+# Close the device
+dev.off
